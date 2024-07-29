8a7a54c41f43cf36e70f2ea6e633ec102ff7795f
Patrick King <slowthought@gmail.com>
2024-07-29 17:14:24 -0400

    Update dwrt.rkt
    Successfully took derivative of self! Next systematically introduce new operators.

--- dwrt.rkt
+++ dwrt.rkt
@@ -1,53 +1,66 @@
 #lang racket
 #| dwrt.rkt -- Macros for finding derivatives. Not complete, just in context of Plato project
    Written by Patrick King, all rights reserved |#
-(require syntax/identifier)
+
 (provide dwrt)
+(require (for-syntax syntax/identifier syntax/parse)
+         racket/math)
+
 ;; Just enough calculus for the Plato project. Assumptions are made in the context of
-;; Lagrangians. Read dwrt as "derivative with respect to"
+;; Lagrangians (bare variables are assumed to be independent). Read dwrt as "derivative
+;; with respect to"
 
+(define-syntax (dwrt stx)
+  
+  (define-syntax-class derivative
+      #:description "valid derivative variable" ; This is terrible. See what errors we get. Do better.
+      (pattern (x:id exp:expr)))
+  
+  (syntax-parse stx
+    [(dwrt x y:number) #'0] ; Constants
+    [(dwrt x y:id)          ; Bare variables
+     (if (eq? (identifier->symbol #'x)
+              (identifier->symbol #'y))
+         #'1 #'0)]
+    
+    ))
+;;     ; Addition
+;;     [(dwrt x (+ y z ...))
+;;      #'(+ (dwrt x (+ z ...))(dwrt x y))]
+;;     [(dwrt x (+ y)) #'(dwrt x y)]
+;;     ; Subtraction/negation
+;;     [(dwrt x (- y z ...))
+;;      #'(- (dwrt x y)(dwrt x (+ z ...)))]
+;;     [(dwrt x (- y))
+;;      #'(- (dwrt x y))]
+;;     ; Multiplication
+;;     [(dwrt x (* y z ...))
+;;      #'(+ (* (dwrt x y) z ...) (* y (dwrt x (* z ...))))]
+;;     [(dwrt x (* y))
+;;      #'(dwrt x y)]
+;;     ; Inversion (division not implemented)
+;;     [(dwrt x (/ y ...))
+;;      #'(- (/ (dwrt x (* y ...)) (sqr (* y ...))))]
+;;     ; Trig
+;;     [(dwrt x (sin y))
+;;      #'(* (cos y)(dwrt x y))]
+;;     [(dwrt x (cos y))
+;;      #'(* -1 (sin y)(dwrt x y))]
+;;     ; Powers
+;;     [(dwrt x (sqr y))
+;;      #'(* 2 y (dwrt x y))]
+;;     [(dwrt x (sqrt y))
+;;      #'(/ (dwrt x y) -2 y (sqrt y))]
 
-(define-syntax (dwrt stx) 
-  (syntax-case stx (+ - * / cos sin sqr sqrt) ; functions to be imported
-    ; Addition
-    [(dwrt x (+ y z ...))
-     #'(+ (dwrt x (+ z ...))(dwrt x y))]
-    [(dwrt x (+ y)) #'(dwrt x y)]
-    ; Subtraction/negation
-    [(dwrt x (- y z ...))
-     #'(- (dwrt x y)(dwrt x (+ z ...)))]
-    [(dwrt x (- y))
-     #'(- (dwrt x y))]
-    ; Multiplication
-    [(dwrt x (* y z ...))
-     #'(+ (* (dwrt x y) z ...) (* y (dwrt x (* z ...))))]
-    [(dwrt x (* y))
-     #'(dwrt x y)]
-    ; Inversion (division not implemented)
-    [(dwrt x (/ y ...))
-     #'(- (/ (dwrt x (* y ...)) (sqr (* y ...))))]
-    ; Trig
-    [(dwrt x (sin y))
-     #'(* (cos y)(dwrt x y))]
-    [(dwrt x (cos y))
-     #'(* -1 (sin y)(dwrt x y))]
-    ; Powers
-    [(dwrt x (sqr y))
-     #'(* 2 y (dwrt x y))]
-    [(dwrt x (sqrt y))
-     #'(/ (dwrt x y) -2 y (sqrt y))]
-    ; Bare numbers and variables
-    [(dwrt x y)
-     (if (identifier? #'x)
-         (if (number? #'y) #'0
-             (if (identifier? #'y)
-                 (if (eq? (identifier->symbol #'x) (identifier->symbol #'y)) #'1 #'0)
-                 (raise-syntax-error #f "2nd argument of unknown/unhandled type" stx)))        
-         (raise-syntax-error #f "first argument not an identifier" stx))]))
 
 (module* test #f
   ;; Variables
   (define x 9)
   (define y x)
   ; Derivative of self should be 1
-  (dwrt x x))
+  (writeln (dwrt x x))
+  ; Derivative of constants or independent variables should be 0
+  (writeln (dwrt x 1))
+  (writeln (dwrt x y)))
+  ; Derivative of sums, is ANY of this working?
+  ;(writeln (dwrt x (+ x y 2))))
